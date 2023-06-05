fbf474cabcea60667104a4ddf149d1119a28c485
Mohammad Tanzeel Ur Rehman <72350513+tanzeelkhan95@users.noreply.github.com>
2023-06-05 20:55:43 +0500

    Made the navigation bar and its related styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
         <div className="me">
-          <img src={ME} alt="me" />
+            <img src={ME} alt="me" />
         </div>
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeAlt2} from 'react-icons/bi'
+import {BiUser} from 'react-icons/bi'
+import {BsBookmarkStar} from 'react-icons/bs'
+import {MdMiscellaneousServices} from 'react-icons/md'
+import {BsChatRight} from 'react-icons/bs'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt2 /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBookmarkStar /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdMiscellaneousServices /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsChatRight /></a>
+    </nav>
   )
 }
 
