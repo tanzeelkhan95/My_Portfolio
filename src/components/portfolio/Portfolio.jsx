3274d3ee48b5fda47edc0a793520c633628978b6
Mohammad Tanzeel Ur Rehman <72350513+tanzeelkhan95@users.noreply.github.com>
2023-06-08 14:24:06 +0500

    Added the portfolio component and its related css. Also made it responsive using media querry

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,38 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpeg'
+
+const Data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Thesis Project',
+    github: 'https://github.com/tanzeelkhan95/Thesis-Project'
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: 'Java FX Project',
+    github: 'https://github.com/tanzeelkhan95/javafx_project'
+  },
+  {
+    id: 1,
+    image: IMG3,
+    title: 'Software Testing with Selenium',
+    github: 'https://github.com/tanzeelkhan95/SoftwareTestingProject'
+  },
+  {
+    id: 1,
+    image: IMG4,
+    title: 'My Portfolio in React',
+    github: 'https://github.com/tanzeelkhan95/my_portfolio'
+  },
+]
+
+
 
 const Portfolio = () => {
   return (
@@ -7,11 +40,21 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
-          <h3>Thesis Project</h3>
-          <a href="https://github.com/tanzeelkhan95/Thesis-Project" className='btn' target='_blank'>Github</a>
-        </article>
+        {
+          Data.map(({id, image, title, github}) => {
+            return (
+              <article key={id} className='portfolio__items'>
+                <div className="portfolio__item-image">
+                  <img src= {image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__items-cta">
+                   <a href={github} className='btn' target='_blank'>Github</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
